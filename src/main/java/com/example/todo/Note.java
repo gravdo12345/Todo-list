172c300ed1279eb2ec6471c17912a8877c24ab6b
GRAVDO <42653608+gravdo12345@users.noreply.github.com>
2024-03-02 22:56:48 +0100

    Add files via upload

---
+++ src/main/java/com/example/todo/Note.java
@@ -0,0 +1,42 @@
+package com.example.todo;
+
+public class Note {
+    private Long id;
+    private String title;
+    private String content;
+
+    // Constructors
+    public Note() {
+    }
+
+    public Note(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
